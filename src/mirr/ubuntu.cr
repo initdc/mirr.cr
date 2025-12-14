7c50365fc5992b134809637ba3e96b479050e76b
initdc <initd@outlook.com>
2025-12-14 17:32:07 +0800

    refactor: move ubuntu scope to ubuntu cli

--- src/mirr/ubuntu.cr
+++ src/mirr/ubuntu.cr
@@ -7,6 +7,83 @@ module Mirr
 
     class MoreThanOne < Exception; end
 
+    class Cli
+      @@ubuntu_help = <<-HELP
+      Usage: mirr ubuntu <command>
+
+      Example:
+        mirr ubuntu change CN
+
+      Command:
+        help                     Show this help message
+        list                     print all ubuntu mirror locations
+        choose                   Asking to choose a server from tcping results
+        fastest                  Use the fastest server from tcping results
+        change [location]        Change source server to location mirror
+        mirror [location]        Use mirror:// protocol with your location
+        custom <url>             Use custom server with your url
+        restore                  Restore sources.list from backup 
+        default                  Recover with default sources.list
+        enable-src               Enable deb-src
+        disable-src              Disable deb-src
+      HELP
+
+      def self.run
+        case ARGV[1]?
+        when "help"
+          puts @@ubuntu_help
+        when "list"
+          Mirr::Ubuntu.print_countries
+        when "choose"
+          location = ARGV[2]?
+          if location
+            Mirr::Ubuntu.choose_server(location)
+          else
+            puts "Please enter a location"
+          end
+        when "fastest"
+          location = ARGV[2]?
+          if location
+            Mirr::Ubuntu.fastest_server(location)
+          else
+            puts "Please enter a location"
+          end
+        when "change"
+          location = ARGV[2]?
+          if location
+            Mirr::Ubuntu.change_server(location)
+          else
+            puts "Please enter a location"
+          end
+        when "mirror"
+          location = ARGV[2]?
+          if location
+            Mirr::Ubuntu.mirror_protocol(location)
+          else
+            puts "Please enter a location"
+          end
+        when "custom"
+          location = ARGV[2]?
+          if location
+            Mirr::Ubuntu.change_custom(location)
+          else
+            puts "Please enter a location"
+          end
+        when "restore"
+          Mirr::Ubuntu.restore_sources
+        when "default"
+          Mirr::Ubuntu.default_conf
+        when "enable-src"
+          Mirr::Ubuntu.enable_deb_src
+        when "disable-src"
+          Mirr::Ubuntu.disable_deb_src
+        else
+          puts "Unknown command"
+          puts @@ubuntu_help
+        end
+      end
+    end
+
     def self.countries : Array(String)
       %w[
         AD.txt
